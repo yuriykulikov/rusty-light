4d40c0a00bd7f4a8edd23513bd0d533a1b7e27e5
yuku8413 <yuriy.kulikov@esolutions.de>
2020-10-16 02:31:50 +0200

    Perceived brightess is f(pwm squared)

--- src/led.rs
+++ src/led.rs
@@ -1,34 +1,36 @@
 use std::cell::Cell;
 
+pub const PWM_MAX: u32 = 100;
+
 /// Power LED of the flashlight. [pwn] represents the duty cycle.
 pub trait Led {
-    fn set_pwm(&self, pwm: u8);
-    fn get_pwm(&self) -> u8;
-    fn modify(&self, f: &dyn Fn(u8) -> u8);
+    fn set_pwm(&self, pwm: u32);
+    fn get_pwm(&self) -> u32;
+    fn modify(&self, f: &dyn Fn(u32) -> u32);
 }
 
 /// Led which resides in memory, for simulation or testing
 pub struct DummyLed {
-    pwm: Cell<u8>
+    pwm: Cell<u32>
 }
 
 impl DummyLed {
     /// Factory function to create a dummy LED
-    pub fn create(pwm: u8) -> Self {
+    pub fn create(pwm: u32) -> Self {
         return DummyLed { pwm: Cell::new(pwm) };
     }
 }
 
 impl Led for DummyLed {
-    fn set_pwm(&self, pwm: u8) {
+    fn set_pwm(&self, pwm: u32) {
         self.pwm.set(pwm);
     }
 
-    fn get_pwm(&self) -> u8 {
+    fn get_pwm(&self) -> u32 {
         return self.pwm.get();
     }
 
-    fn modify(&self, f: &dyn Fn(u8) -> u8) {
+    fn modify(&self, f: &dyn Fn(u32) -> u32) {
         let prev = self.get_pwm();
         let value = f(prev);
         self.set_pwm(value)
