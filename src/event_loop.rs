e178ad8b3b40c845a949a1f501b8aaaebe22ccac
yuku8413 <yuriy.kulikov@esolutions.de>
2020-10-22 15:58:24 +0200

    Moved edt to main and added a test for control

--- src/event_loop.rs
+++ src/event_loop.rs
@@ -9,49 +9,45 @@ struct Msg<T> {
 pub struct EDT<T> {
     now: Cell<u32>,
     queue: RefCell<Vec<Msg<T>>>,
-    sleeper: fn(u32) -> (),
 }
 
 impl<T> EDT<T> {
-    pub fn create(sleeper: fn(u32) -> ()) -> EDT<T> {
+    pub fn create() -> EDT<T> {
         EDT {
             now: Cell::new(0),
             queue: RefCell::new(Vec::new()),
-            sleeper,
         }
     }
 }
 
-/// TODO use a handler function instead of poll
-/// TODO make sleep and now injectable
-/// TODO no std
 impl<T> EDT<T> {
     pub fn exit(&self) {
         // clearing the queue ends the loop
         self.queue.borrow_mut().clear();
     }
 
-    pub fn process_events(&self, handler: &dyn Fn(T)) {
+    /// Processes all events which are due within the advance_time_by from now
+    /// returns the time until the next event, which can be used to sleep
+    pub fn process_events(&self, advance_time_by: u32, handler: &dyn Fn(T)) -> u32 {
+        let target_time = self.now.get() + advance_time_by;
         loop {
             if self.queue.borrow().is_empty() {
-                break;
+                return 0;
+            }
+
+            self.queue.borrow_mut().sort_by_key(|msg| { msg.when });
+
+            let next_when = self.queue.borrow().first().unwrap().when;
+
+            if next_when > target_time {
+                // processed all messages due before target_time
+                self.now.set(target_time);
+                return next_when - target_time;
             } else {
-                self.queue
-                    .borrow_mut()
-                    .sort_by_key(|msg| { msg.when });
-
-                let first = self.queue
-                    .borrow_mut()
-                    .remove(0);
-
-                let millis = first.when - self.now.get();
-
-                (self.sleeper)(millis);
-
-                self.now.set(first.when);
-
-                handler(first.payload);
-            };
+                let next_msg = self.queue.borrow_mut().remove(0);
+                self.now.set(next_msg.when);
+                handler(next_msg.payload);
+            }
         }
     }
 
@@ -75,7 +71,7 @@ mod tests {
 
     #[test]
     fn send_some_events() {
-        let edt: EDT<u32> = EDT::create(|_| { /* NOP */ });
+        let edt: EDT<u32> = EDT::create();
         edt.schedule(1000, 1);
         edt.schedule(3000, 2);
         assert_eq!(edt.now.get(), 0);
