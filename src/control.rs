e3f39725de3d76f5acffdcde1546c36bffce0f10
yuku8413 <yuriy.kulikov@esolutions.de>
2020-10-26 14:49:55 +0100

    Created with_bench function for test

--- src/control.rs
+++ src/control.rs
@@ -102,11 +102,59 @@ impl<'a, P: Pin> LightControl<'a, P> {
 mod tests {
     use std::cell::Cell;
 
-    use crate::control::{LightControl};
+    use crate::control::LightControl;
     use crate::event_loop::EDT;
-    use crate::led::{DummyLed, Led, PWM_MAX};
+    use crate::led::{Led, PWM_MAX};
     use crate::pin::Pin;
     use crate::rgb::{DummyRgb, Rgb};
+
+    #[test]
+    fn button_clicks_change_brightness() {
+        with_bench(&|advance_time, buttons, pwm| {
+            buttons.press_plus();
+            advance_time(800);
+            assert_eq!(pwm.get(), 100);
+            buttons.release_plus();
+
+            buttons.press_minus();
+            advance_time(800);
+            buttons.release_minus();
+            assert_eq!(pwm.get(), 0);
+        });
+    }
+
+    fn with_bench(block: &dyn Fn(&dyn Fn(u32), Buttons, &Cell<u32>)) {
+        let plus_pin = Cell::new(false);
+        let minus_pin = Cell::new(false);
+        let pwm = Cell::new(0);
+        let led = TestLed { pwm: &pwm };
+        let rgb = DummyRgb::create();
+        let edt = EDT::create();
+        let light_control = LightControl {
+            plus_pin: TestPin { is_down: &plus_pin },
+            minus_pin: TestPin { is_down: &minus_pin },
+            led: &led,
+            edt: &edt,
+            rgb: &rgb,
+        };
+        light_control.check_buttons();
+
+        let advance_time = |time: u32| {
+            edt.process_events(time, &|action| {
+                light_control.process_message(action);
+                render_flashlight_state(led.get_pwm(), rgb.get_rgb());
+            });
+        };
+
+        block(&advance_time, Buttons { plus_pin: &plus_pin, minus_pin: &minus_pin }, &pwm);
+    }
+
+    fn render_flashlight_state(pwm: u32, rgb: u8) {
+        let mut led_str = String::new();
+        for _ in 0..pwm { led_str.push('*'); }
+        for _ in 0..(PWM_MAX - pwm) { led_str.push(' '); }
+        println!("  [{}]  [{}]", led_str, rgb);
+    }
 
     struct TestPin<'a> {
         is_down: &'a Cell<bool>,
@@ -119,42 +167,36 @@ mod tests {
         }
     }
 
-    /// TODO extract setup function
-    #[test]
-    fn button_clicks_change_brightness<'t>() {
-        let plus_pin = Cell::new(false);
-        let minus_pin = Cell::new(false);
-        let led = DummyLed::create(0);
-        let rgb = DummyRgb::create();
-        let edt = EDT::create();
-        let light_control = LightControl {
-            plus_pin: TestPin { is_down: &plus_pin },
-            minus_pin: TestPin { is_down: &minus_pin },
-            led: &led,
-            edt: &edt,
-            rgb: &rgb,
-        };
-        light_control.check_buttons();
-
-        let on_next_event = &|action| {
-            light_control.process_message(action);
-            render_flashlight_state(led.get_pwm(), rgb.get_rgb());
-        };
-
-        plus_pin.set(true);
-        edt.process_events(800, on_next_event);
-        assert_eq!(led.get_pwm(), 100);
-
-        plus_pin.set(false);
-        minus_pin.set(true);
-        edt.process_events(800, on_next_event);
-        assert_eq!(led.get_pwm(), 0);
+    /// Led which resides in memory, for simulation or testing
+    pub struct TestLed<'a> {
+        pub pwm: &'a Cell<u32>
     }
 
-    fn render_flashlight_state(pwm: u32, rgb: u8) {
-        let mut led_str = String::new();
-        for _ in 0..pwm { led_str.push('*'); }
-        for _ in 0..(PWM_MAX - pwm) { led_str.push(' '); }
-        println!("  [{}]  [{}]", led_str, rgb);
+    impl<'a> Led for TestLed<'a> {
+        fn set_pwm(&self, pwm: u32) {
+            self.pwm.set(pwm);
+        }
+
+        fn get_pwm(&self) -> u32 {
+            return self.pwm.get();
+        }
+
+        fn modify(&self, f: &dyn Fn(u32) -> u32) {
+            let prev = self.get_pwm();
+            let value = f(prev);
+            self.set_pwm(value)
+        }
+    }
+
+    pub struct Buttons<'a> {
+        plus_pin: &'a Cell<bool>,
+        minus_pin: &'a Cell<bool>,
+    }
+
+    impl<'a> Buttons<'a> {
+        fn press_plus(&self) { self.plus_pin.set(true); }
+        fn release_plus(&self) { self.plus_pin.set(false); }
+        fn press_minus(&self) { self.minus_pin.set(true); }
+        fn release_minus(&self) { self.minus_pin.set(false); }
     }
 }
