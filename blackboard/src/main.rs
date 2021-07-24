6864f38a6a7901921a6713897cffd74a1412b312
Yuriy Kulikov <yuriy.kulikov.87@gmail.com>
2021-07-24 22:11:13 +0200

    Implemented simple PWM glow

--- blackboard/src/main.rs
+++ blackboard/src/main.rs
@@ -13,7 +13,11 @@ use core::fmt::Write;
 use hal::prelude::*;
 use hal::stm32;
 use nb::block;
-use rt::{entry, ExceptionFrame, exception};
+use rt::{entry, exception, ExceptionFrame};
+
+use light_control::control::{Action, LightControl};
+use light_control::control::Action::{Blink, SetPwm};
+use light_control::edt::{EDT, Event};
 
 #[entry]
 fn main() -> ! {
@@ -32,30 +36,71 @@ fn main() -> ! {
     let mut pwm_ch1 = pwm.bind_pin(gpiob.pb3);
 
     let max = pwm_ch1.get_max_duty();
-    pwm_ch1.set_duty(max / 2);
+
+    let mut duties: [u16; 101] = [0; 101];
+    for i in 0..=100 {
+        let duh = i as u32;
+        let m = max as u32;
+        let duty = if i < 8 { m * duh / 903 } else { m * (duh + 16) / 116 * (duh + 16) / 116 * (duh + 16) / 116 };
+        duties[i] = duty as u16;
+    }
+
+    pwm_ch1.set_duty(0);
     pwm_ch1.enable();
 
     // let mut watchdog = dp.WWDG.constrain(&mut rcc);
     let mut watchdog = dp.IWDG.constrain();
-    watchdog.start(1000.ms());
+    watchdog.start(2000.ms());
 
     let mut timer = dp.TIM17.timer(&mut rcc);
-    timer.start(250.ms());
 
+    let mut down = false;
     loop {
-        watchdog.feed();
-        let _ = writeln!(output, "Blink!");
-        green_led.set_high().unwrap();
-        for i in 1..4 {
-            block!(timer.wait()).unwrap();
-            pwm_ch1.set_duty(max / i);
-        }
-        green_led.set_low().unwrap();
-        for i in (1..4).rev() {
-            block!(timer.wait()).unwrap();
-            pwm_ch1.set_duty(max / i);
+        match edt.poll() {
+            Event::Execute { msg } => {
+                watchdog.feed();
+                writeln!(output, "Action: {:?}", msg);
+                match msg {
+                    Action::Blink { color, blinks } => {
+                        if blinks == 1 {
+                            green_led.set_high().unwrap();
+                            edt.schedule(500, Blink { color: 0, blinks: 0 });
+                        } else {
+                            green_led.set_low().unwrap();
+                            edt.schedule(500, Blink { color: 0, blinks: 1 });
+                        }
+                    }
+                    Action::SetPwm { power_level: goal } => {
+                        let duty = duties[goal as usize];
+                        // writeln!(output, "Duty: {}", duty);
+                        pwm_ch1.set_duty(max - duty);
+                        if goal == 100 {
+                            down = true;
+                            edt.schedule(10, SetPwm { power_level: 99 });
+                        } else if goal == 0 {
+                            down = false;
+                            edt.schedule(10, SetPwm { power_level: 1 });
+                        } else if down {
+                            edt.schedule(10, SetPwm { power_level: goal - 1 });
+                        } else {
+                            edt.schedule(10, SetPwm { power_level: goal + 1 });
+                        }
+                    }
+                    _ => {}
+                }
+            }
+            Event::Wait { ms } => {
+                writeln!(output, "Going to sleep: {}", ms);
+                timer.start(ms.ms());
+                block!(timer.wait()).unwrap();
+            }
+            Event::Halt => {
+                break;
+            }
         }
     }
+    writeln!(output, "Halted!");
+    panic!("Halted!");
 }
 
 #[exception]
